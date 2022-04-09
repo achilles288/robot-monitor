b8f27239ec22c500587d1b223b3758527f4ff935
khantkyawkhaung <khantkyawkhaung288@gmail.com>
2022-04-09 23:47:09 +0630

    :bento: Float output format

--- client/src/rm_output.c
+++ client/src/rm_output.c
@@ -12,7 +12,10 @@
 
 #include "connection_private.h"
 
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
 /**
@@ -28,11 +31,14 @@ void rmOutputAttributeUpdate(rmOutputAttribute *attr) {
     char msg[128];
     char* str = rmOutputAttributeGetStringData(attr);
     snprintf(msg, 128, "$set %s %s\n", attr->name, str);
-    rmSendMessage(msg);
+    _rmSendMessage(msg);
 }
 
 
-static char buffer[20];
+#define BUFFER_SIZE 48
+
+static char strBuffer[BUFFER_SIZE + 1];
+static uint8_t strIndex = 0;
 
 
 /**
@@ -45,19 +51,28 @@ static char buffer[20];
  *         allocated in a temporary memory.
  */
 char* rmOutputAttributeGetStringData(rmOutputAttribute *attr) {
+    char* ptr = &strBuffer[strIndex];
+    uint8_t size = BUFFER_SIZE - strIndex;
+    if(size < 1) {
+        strIndex = 0;
+        ptr = &strBuffer[0];
+    }
+    
     if(attr->type == RM_ATTRIBUTE_BOOL) {
         bool* b = (bool*) attr->data;
         if(*b)
-            buffer[0] = '1';
+            ptr[0] = '1';
         else
-            buffer[0] = '0';
-        buffer[1] = '\0';
+            ptr[0] = '0';
+        ptr[1] = '\0';
+        strIndex += 2;
     }
     
     else if(attr->type == RM_ATTRIBUTE_CHAR) {
         char* c = (char*) attr->data;
-        buffer[0] = *c;
-        buffer[1] = '\0';
+        ptr[0] = *c;
+        ptr[1] = '\0';
+        strIndex += 2;
     }
     
     else if(attr->type == RM_ATTRIBUTE_STRING) {
@@ -67,51 +82,110 @@ char* rmOutputAttributeGetStringData(rmOutputAttribute *attr) {
     
     else if(attr->type == RM_ATTRIBUTE_FLOAT) {
         float* f = (float*) attr->data;
-        int d = (int) *f;
-        uint16_t rem;
-        if(d >= 1000) {
-            rem = (int) ((*f - d) * 1000);
-            if(rem < 0)
-                rem *= -1;
-            sprintf(buffer, "%d.%03u", d, rem);
-        }
-        else {
-            rem = (int) ((*f - d) * 1000000);
-            if(rem < 0)
-                rem *= -1;
-            sprintf(buffer, "%d.%06u", d, rem);
-        }
+        return rmFloatToString(*f);
     }
     
     else if(attr->type & 0b00010000) {
+        if(size < 11) {
+            strIndex = 0;
+            ptr = &strBuffer[0];
+        }
+        uint8_t len = 0;
         if(attr->type & 0b00001000) {
             if(attr->type == RM_ATTRIBUTE_INT8) {
                 int8_t* i = (int8_t*) attr->data;
-                sprintf(buffer, "%d", *i);
+                len = sprintf(ptr, "%d", *i);
             }
             else if(attr->type == RM_ATTRIBUTE_INT16) {
                 int16_t* i = (int16_t*) attr->data;
-                sprintf(buffer, "%d", *i);
+                len = sprintf(ptr, "%d", *i);
             }
             else if(attr->type == RM_ATTRIBUTE_INT32) {
                 int32_t* i = (int32_t*) attr->data;
-                sprintf(buffer, "%ld", (long) *i);
+                len = sprintf(ptr, "%ld", (long) *i);
             }
         }
         else {
             if(attr->type == RM_ATTRIBUTE_UINT8) {
                 uint8_t* i = (uint8_t*) attr->data;
-                sprintf(buffer, "%u", *i);
+                len = sprintf(ptr, "%u", *i);
             }
             else if(attr->type == RM_ATTRIBUTE_UINT16) {
                 uint16_t* i = (uint16_t*) attr->data;
-                sprintf(buffer, "%u", *i);
+                len = sprintf(ptr, "%u", *i);
             }
             else if(attr->type == RM_ATTRIBUTE_UINT32) {
                 uint32_t* i = (uint32_t*) attr->data;
-                sprintf(buffer, "%lu", (unsigned long) *i);
+                len = sprintf(ptr, "%lu", (unsigned long) *i);
             }
         }
+        strIndex = strIndex + len + 1;
     }
-    return buffer;
+    return ptr;
+}
+
+
+/**
+ * @brief Converts the floating point number to a string
+ * 
+ * @param f The floating point value
+ * 
+ * @return String representation of the float number. The string returned
+ *         must be used or copied before some next function calls since it is
+ *         allocated in a temporary memory.
+ */
+char* rmFloatToString(float f) {
+    char* ptr = &strBuffer[strIndex];
+    uint8_t size = BUFFER_SIZE - strIndex;
+    if(size < 11) {
+        strIndex = 0;
+        ptr = &strBuffer[0];
+    }
+    
+    uint8_t i = 0;
+    if(f < 0) {
+        ptr[i++] = '-';
+        f = -f;
+    }
+    uint32_t d = abs((int) f);
+    if(f < 0.001f) {
+        float q = f;
+        int8_t p = 0;
+        do {
+            q *= 10;
+            p--;
+        } while(q < 1.0f);
+        uint8_t s = (uint8_t) q;
+        uint16_t r = (uint16_t) ((q - s + 0.0005f) * 1000);
+        i += sprintf(&ptr[i], "%u.%03ue%d", s, r, p);
+    }
+    else if(d == 0) {
+        uint32_t rem = (uint32_t) ((f - d + 0.0000005f) * 1e6);
+        i += sprintf(&ptr[i], "0.%06lu", rem);
+    }
+    else if(d < 1000) {
+        uint32_t rem = (uint32_t) ((f - d + 0.0000005f) * 1e6);
+        i += sprintf(&ptr[i], "%lu.%06lu", d, rem);
+    }
+    else if(d < 1000000) {
+        uint16_t rem = (uint16_t) ((f - d + 0.0005f) * 1000);
+        i += sprintf(&ptr[i], "%lu.%03u", d, rem);
+    }
+    else if(!isnan(f)) {
+        float q = f;
+        int8_t p = 0;
+        do {
+            q /= 10;
+            p++;
+        } while(q >= 10.0f);
+        uint8_t s = (uint8_t) q;
+        uint16_t r = (uint16_t) ((q - s + 0.0005f) * 1000);
+        i += sprintf(&ptr[i], "%u.%03ue%d", s, r, p);
+    }
+    else {
+        strcpy(&ptr[i], "nan");
+        i += 3;
+    }
+    strIndex = strIndex + i + 1;
+    return ptr;
 }
