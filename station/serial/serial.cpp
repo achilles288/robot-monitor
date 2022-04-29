8012dd16e89184c315dcca638657e67151ac8d93
khantkyawkhaung <khantkyawkhaung288@gmail.com>
2022-04-29 16:02:27 +0630

    :truck: Placed the serial library together with the source

--- station/serial/serial.cpp
+++ station/serial/serial.cpp
@@ -9,12 +9,12 @@
 # define alloca __builtin_alloca
 #endif
 
-#include "serial/serial.h"
+#include "serial.h"
 
 #ifdef _WIN32
-#include "serial/impl/win.h"
+#include "impl/win.h"
 #else
-#include "serial/impl/unix.h"
+#include "impl/unix.h"
 #endif
 
 using std::invalid_argument;
